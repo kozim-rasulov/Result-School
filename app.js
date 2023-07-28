cf55f49d8f99ccdbb030ce524aad077987006280
Kozim <kozim_7484443@mail.ru>
2023-07-28 16:27:33 +0500

    Working with Drag & Drop

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-function slidesPlugin(activeSlide = 0) {
-    const slides = document.querySelectorAll('.slide');
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
-    slides[activeSlide].classList.add('active');
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
 
-    for (const slide of slides) {
-        slide.addEventListener('click', () => {
-            clearActiveClasses();
-            slide.classList.add('active');
-        })
-    }
-
-    function clearActiveClasses() {
-        slides.forEach((slide) => {
-            slide.classList.remove('active');
-        })
-    }
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
 }
 
-slidesPlugin();
+function dragstart(event) {
+    event.target.classList.add('hold');
+    setTimeout(() => event.target.classList.add
+    ('hide'), 0)
+    
+}
+
+function dragend(event) {
+    event.target.className = 'item';
+}
+
+
+
+function dragover(event) {
+    event.preventDefault();
+}
+function dragenter(event) {
+    event.target.classList.add('hovered');
+    
+}
+function dragleave(event) {
+    event.target.classList.remove('hovered');
+    
+}
+function dragdrop(event) {
+    event.target.append(item);
+    event.target.classList.remove('hovered');
+}
